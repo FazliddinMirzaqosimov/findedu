f17fc3be34067efd3472e1024061cf297e67f04d
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-02-05 12:07:09 +0500

    bug fixed

--- src/pages/auth/Signin/index.js
+++ src/pages/auth/Signin/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './index.style.less';
 import AuthWrapper from '../AuthWrapper';
 import AppPageMetadata from '../../../@crema/core/AppPageMetadata';
-// import SignInFirebase from './SigninFirebase';
+import SignInFirebase from './SigninFirebase';
 
 const Signin = () => {
   return (
     <AuthWrapper>
       <AppPageMetadata title='Login' />
-      {/* <SignInFireb  ase /> */}
+      <SignInFirebase />
     </AuthWrapper>
   );
 };
